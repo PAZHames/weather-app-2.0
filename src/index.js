2058082771c28e67903c7be3996cda0a06c5e0ac
PAZHames <77605055+PAZHames@users.noreply.github.com>
2022-10-04 15:28:05 +0100

    update forecast to draw data from api and display day, rounded max/min temp, icon
    show only next 6 days

--- src/index.js
+++ src/index.js
@@ -55,25 +55,44 @@ function displayWeather(response) {
   getForecast(response.data.coord);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thurs", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
             <div class="card">
               <div class="card-body">
-                <div>${day}</div>
-                <i class="fa-solid fa-sun"></i>
+                <div>${formatDay(forecastDay.dt)}</div>
+                
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" class="forecast-icon">
+
                 <br>
-                <span class="forecast-temp-max">15°</span>
-                <span class="forecast-temp-min">10°</span>
+                <span class="forecast-temp-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="forecast-temp-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
               </div>
             </div>
           </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
